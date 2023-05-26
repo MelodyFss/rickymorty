01e0cc4452ada982c214c9b00350c82d013f0c6e
MelodyFss <melodyponczko@gmail.com>
2023-05-26 11:36:35 -0300

    cambios

--- Server/src/controllers/getCharById.js
+++ Server/src/controllers/getCharById.js
@@ -1,27 +1,30 @@
 const axios = require("axios");
 const URL = "https://rickandmortyapi.com/api/character"
 
-const getCharById = (req, res) => {
-    const { id } = req.params;
+const getCharById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { data } = await axios(`${URL}/${id}`)
 
-    axios.get(`${URL}/${id}`)
-        .then((result) => result.data)
-        .then(({ name, gender, origin, status, image, species }) => {
-            if (name) {
-                let character = {
-                    id,
-                    name,
-                    gender,
-                    origin,
-                    image,
-                    status,
-                    species
-                }
-                return res.status(200).json(character)
-            }
-            return res.status(404).send('Not found')
-        })
-        .catch((error) => res.status(500).send(error.message))
+        if(!data.name) throw Error(`Faltan datos del personaje con ID: ${id}`);
+
+        const character = {
+            id: data.id,
+            name: data.name,
+                    gender: data.gender,
+                    origin: data.origin,
+                    image: data.image,
+                    status: data.status,
+                    species: data.species
+        }
+        return res.status(200).json(character)
+    }
+    catch (error){
+        return error.message.includes('ID')
+        ? res.status(404).send(error.message)
+        : res.status(500).send(error.message)
+    }
+    
 };
 
 module.exports = {
